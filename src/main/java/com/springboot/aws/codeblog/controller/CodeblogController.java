da69e48bdcbd9e2824cec8317584e8270bb7536e
RuiJ72 <rlagos900@gmail.com>
2024-05-26 12:37:13 -0300

    Inserting summernote layout on Post page

--- src/main/java/com/springboot/aws/codeblog/controller/CodeblogController.java
+++ src/main/java/com/springboot/aws/codeblog/controller/CodeblogController.java
@@ -4,11 +4,15 @@ import com.springboot.aws.codeblog.model.Post;
 import com.springboot.aws.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -24,6 +28,7 @@ public class CodeblogController {
         mv.addObject("posts", posts);
         return mv;
     }
+
     @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
     public ModelAndView getPostDetails(@PathVariable("id") long id) {
         ModelAndView mv = new ModelAndView("postDetails");
@@ -32,4 +37,19 @@ public class CodeblogController {
         return mv;
     }
 
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm() {
+        return "postForm";
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            attributes.addFlashAttribute("message", "Make sure all fields are filled in");
+            return "redirect:/newpost";
+        }
+        post.setDate(LocalDate.now());
+        codeblogService.save(post);
+        return "redirect:/posts";
+    }
 }
